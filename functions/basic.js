26a19e48e1f5584b210b37e72a1da9795ef26992
nhatkytran <nhatky.tran.2002@gmail.com>
2022-06-26 15:28:11 +0700

    Access Control

--- functions/basic.js
+++ functions/basic.js
@@ -2,6 +2,9 @@ const students = require('../assets/students');
 
 exports.handler = async function (event, context) {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(students),
   };
--- functions/first.js
+++ functions/first.js
@@ -11,6 +11,9 @@ const students = [
 
 exports.handler = async function (event, context) {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(students),
   };
